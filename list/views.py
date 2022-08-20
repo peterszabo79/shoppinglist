2bd9b2b6e83edc718df9b97c20facc592429485c
peterszabo79 <peterszabo79@gmail.com>
2022-08-20 19:46:31 +0000

    add search bar

--- list/views.py
+++ list/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -26,12 +29,16 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = "__all__"
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('task')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = "__all__"
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('task')
 
 class TaskDelete(LoginRequiredMixin, DeleteView):
@@ -46,3 +53,20 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('task')
+
+class RegisterPage(FormView):
+    template_name = 'list/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('task')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task')
+        return super(RegisterPage, self).get(*args, *kwargs)
