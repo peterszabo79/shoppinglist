c37524bce57b77e58d531ed797862edb06fa351f
peterszabo79 <peterszabo79@gmail.com>
2022-08-20 18:08:40 +0000

    add form and delete html,add views

--- list/views.py
+++ list/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
+from django.urls import reverse_lazy
 
 
 class TaskList(ListView):
@@ -11,3 +13,20 @@ class TaskList(ListView):
 
 class TaskDetail(DetailView):
     model = Task
+    context_object_name = 'task'
+    template_name = 'list/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('task')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('task')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('task')
