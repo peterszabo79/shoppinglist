9d90e216fa291537811d507e30b052ecbf0fb4cd
peterszabo79 <peterszabo79@gmail.com>
2022-08-20 17:00:34 +0000

    add task html and tasklist html

--- list/views.py
+++ list/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
-# Create your views here.
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'task'
+
+class TaskDetail(DetailView):
+    model = Task
